780af4a4407290e876bf2e366e4712f9abb3d46c
Baruch Rotbart <baruchrotbart@baruchs-mbp.lan>
2021-11-14 13:11:58 -0600

    pre exercise

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-import re
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,32 +11,51 @@ app = Flask(__name__)
 #     user_logged_in = False
 #     return render_template('basic.html', name=name, letters=letters, pup_dictionary=pup_dictionary, puppies=puppies, user_logged_in=user_logged_in)
 
-@app.route('/information')
-def info():
-    return "<h1>Puppies are cute!</h1>"
+# @app.route('/information')
+# def info():
+#     return "<h1>Puppies are cute!</h1>"
 
 # @app.route('/puppy/<name>')
 # def puppy(name):
 #     return "100th Letter: {}".format(name[100])
 
-@app.route('/puppy_name/<name>')
-def puppylatin(name):
-    pupname = ''
-    if name[-1] =='y':
-        pupname =name[:-1] + 'iful'
-    else:
-        pupname = name + 'y'
+# @app.route('/puppy_name/<name>')
+# def puppylatin(name):
+#     pupname = ''
+#     if name[-1] =='y':
+#         pupname =name[:-1] + 'iful'
+#     else:
+#         pupname = name + 'y'
     
-    return f"Hello {name} Your puppy latin name is: {pupname}"
+#     return f"Hello {name} Your puppy latin name is: {pupname}"
+
+# @app.route('/')
+# def index():
+#     return render_template('home.html')
+
+# @app.route('/puppy/<name>')
+# def pup_name(name):
+#     return render_template('puppy.html', name=name)
+
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/puppy/<name>')
-def pup_name(name):
-    return render_template('puppy.html', name=name)
+@app.route('/signup_form')
+def signup_form():
+    return render_template('signup.html')
 
+@app.route('/thank_you')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+
+    return render_template('thankyou.html', first=first, last=last)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__=='__main__':
     app.run(debug=True)
