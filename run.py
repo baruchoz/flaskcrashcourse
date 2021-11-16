f25549691f7edaf9462d769a0b5ae03eae79f5c7
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2021-11-15 21:35:02 -0600

    about to start flask db's

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import (StringField, BooleanField, DateTimeField, 
+                    RadioField, SelectField, TextField,
+                    TextAreaField, SubmitField)
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='mysecretkey'
+
+# 1st- Flask Basics
 
 # @app.route('/')
 # def indexpage():
@@ -38,24 +48,67 @@ app = Flask(__name__)
 #     return render_template('puppy.html', name=name)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/signup_form')
-def signup_form():
-    return render_template('signup.html')
+# Second - Flask Templates
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/signup_form')
+# def signup_form():
+#     return render_template('signup.html')
+
+# @app.route('/thank_you')
+# def thank_you():
+#     first = request.args.get('first')
+#     last = request.args.get('last')
+
+#     return render_template('thankyou.html', first=first, last=last)
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+
+
+# 3rd - Flask Forms
+
+class InfoForm(FlaskForm):
+
+    breed = StringField("What breed are you?", validators=[DataRequired()])
+    neutered = BooleanField("Have you been neutered?")
+    mood = RadioField('Please choose your mood:', choices=[('mood_one', 'Happy'),('mood_two','Excited')])
+    food_choice = SelectField(u'Pick your favorite food:', choices=[('chi','Chicken'),('bf','Beef'),('fi','Fish')])
+    feedback = TextAreaField()
+    submit = SubmitField("Submit")
+    
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     breed = False
+#     form =InfoForm()
+#     if form.validate_on_submit():
+#         breed = form.breed.data
+#         form.breed.data = ''
+#     return render_template('index.html', form=form, breed=breed)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form =InfoForm()
+    if form.validate_on_submit():
+        session['breeed'] = form.breed.data
+        session['neutered'] = form.neutered.data
+        session['mood'] = form.mood.data
+        session['food'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
+
+        return redirect(url_for('thank_you'))
+
+    return render_template('index.html',form=form)
 
 @app.route('/thank_you')
 def thank_you():
-    first = request.args.get('first')
-    last = request.args.get('last')
-
-    return render_template('thankyou.html', first=first, last=last)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('thankyou.html')
 
 if __name__=='__main__':
     app.run(debug=True)
